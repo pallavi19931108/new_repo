a1860b99a65bb865a5df160d21660965354ee281
pallavi19931108 <pallavi.shimpi16@gmail.com>
2024-03-25 07:25:35 +0530

    this is my fun program

---
+++ list.py
@@ -0,0 +1,8 @@
+def my_function(fname):
+    print(fname + "patil")
+my_function("santosh")
+my_function("ravina")
+my_function("rohan")
+def fun(*kid):
+     print("The younger child ic:",kid[3])
+fun("sulabha","rani","raj","sanu")
